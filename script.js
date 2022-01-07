38b9831bd18637e4d64f72f7a9f94fea39e80f73
kevinbdx35 <k.bardoux@orange.fr>
2022-01-07 15:00:40 +0100

    init commit 🐶

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach(function (label) {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
+
+/*labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});*/
